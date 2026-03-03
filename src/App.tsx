64bc0270e83fe2ef3f61bb371771bfafe20ce687
sindy970 <sindy970@naver.com>
2026-03-03 13:55:46 +0900

    메뉴 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import AboutMe from './components/About/AboutMe.tsx';
+// import AboutMe from './components/About/AboutMe.tsx';
 import Terminal from './components/Terminal/Terminal.tsx';
 import AbountMeCard from './components/AboutMe/AbountMeCard.tsx';
 import MetroTimeline from './components/Career/MetroTimeline.tsx';
-import CardStack from './components/Career/CardStack.tsx';
+// import CardStack from './components/Career/CardStack.tsx';
 import CareerDetail from './components/Career/CareerDetail.tsx';
+import logo from './assets/logoIcon.webp';
 import type {CareerData} from './types/career.ts';
 import {useEffect, useState} from "react";
 
@@ -28,15 +29,15 @@ function App() {
             <div className="min-h-screen bg-dark-bg text-brand-white">
                 <nav className="fixed top-0 w-full z-50 flex items-center justify-between px-8 py-5 bg-black/40 backdrop-blur-xl border-b border-white/5">
                     <Link to="/" className="text-2xl font-black text-brand-main cursor-pointer tracking-tighter">
-                        PORTFOLIO
+                        <img className="w-10" src={logo}/>
                     </Link>
                     <ul className="flex gap-10 font-bold text-sm uppercase tracking-widest">
-                        <li><Link to="/about" className="hover:text-brand-main text-gray-400">About</Link></li>
-                        <li><Link to="/projects" className="hover:text-brand-main text-gray-400">Projects</Link></li>
+                        {/*<li><Link to="/about" className="hover:text-brand-main text-gray-400">About</Link></li>*/}
                         <li><Link to="/aboutMe" className="hover:text-brand-main text-gray-400">AboutMe</Link></li>
                         <li><Link to="/metroTimeline" className="hover:text-brand-main text-gray-400">MetroTimeline</Link></li>
-                        <li><Link to="/cardStack" className="hover:text-brand-main text-gray-400">CardStack</Link></li>
                         <li><Link to="/careerDetail" className="hover:text-brand-main text-gray-400">CareerDetail</Link></li>
+                        <li><Link to="/projects" className="hover:text-brand-main text-gray-400">Projects</Link></li>
+                        {/*<li><Link to="/cardStack" className="hover:text-brand-main text-gray-400">CardStack</Link></li>*/}
                     </ul>
                 </nav>
 
@@ -51,12 +52,12 @@ function App() {
                                 <div className="mt-20 h-1 w-24 bg-brand-sub rounded-full opacity-50 shadow-[0_0_15px_#FFE69A]" />
                             </div>
                         } />
-                        <Route path="/about" element={<AboutMe />} />
-                        <Route path="/projects" element={<Terminal />} />
+                        {/*<Route path="/about" element={<AboutMe />} />*/}
                         <Route path="/aboutMe" element={<AbountMeCard />} />
                         <Route path="/metroTimeline" element={<MetroTimeline data={data} />} />
-                        <Route path="/cardStack" element={<CardStack data={data} />} />
                         <Route path="/careerDetail" element={<CareerDetail />} />
+                        <Route path="/projects" element={<Terminal />} />
+                        {/*<Route path="/cardStack" element={<CardStack data={data} />} />*/}
                     </Routes>
                 </main>
             </div>
